d2e050aab3b7cdc1f3197fc11f28f569c58edc16
snort12 <118768699+snort12@users.noreply.github.com>
2023-09-21 10:36:35 +0200

    #13 terraform random provider (#14)

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-# First commit
-# second change and commit
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length   = 16
+  special  = false
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
