01790557bfb55e186869dc9f9408a0737377f82b
snort12 <118768699+snort12@users.noreply.github.com>
2023-09-25 14:27:36 +0200

    #23 setting up root module structure (#24)

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  cloud {
+    organization = "Snort"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
+  # Configuration options
+}
