130fc169fb64469a4f545fcf89152ab1433c987a
snort12 Snort <snort@data.pl>
2023-09-26 13:02:27 +0000

    #25 bucket should no longer use random provider

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
 
-resource "random_string" "bucket_name" {
-  lower   = true
-  upper   = false
-  length  = 16
-  special = false
-}
-
-resource "aws_s3_bucket" "simple_bucket" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = random_string.bucket_name.result
+  bucket = var.bucket_name
   
   tags = {
     UserUuid = var.user_uuid
