5d020c2a48b61387a195bf2f397dea6165a649f0
snort12 Snort <snort@data.pl>
2023-09-27 12:19:33 +0000

    #31 fixing formatting

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
- # cloud {
- #   organization = "Snort"
- #
- #   workspaces {
- #     name = "terra-house-1"
- #   }
- # }
+  # cloud {
+  #   organization = "Snort"
+  #
+  #   workspaces {
+  #     name = "terra-house-1"
+  #   }
+  # }
 
   required_providers {
     aws = {
@@ -20,9 +20,9 @@ provider "aws" {
 }
 
 module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
+  source              = "./modules/terrahouse_aws"
+  user_uuid           = var.user_uuid
+  bucket_name         = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
 }
