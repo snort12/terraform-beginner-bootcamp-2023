b38beee4660f92cdf520c72c14df94c626387e08
snort12 <118768699+snort12@users.noreply.github.com>
2023-10-05 17:33:54 +0200

    #50 fix issues with provider, setup two different towns. (#51)

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
       version = "5.17.0"
     }
     terratowns = {
-      source = "local.providers/local/terratowns"
+      source  = "local.providers/local/terratowns"
       version = "1.0.0"
     }
   }
@@ -23,27 +23,41 @@ provider "aws" {
 }
 
 provider "terratowns" {
-  endpoint = var.terratowns_endpoint
+  endpoint  = var.terratowns_endpoint
   user_uuid = var.teacherseat_user_uuid
-  token = var.terratowns_access_token
+  token     = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.teacherseat_user_uuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version     = var.content_version
-  assets_path         = var.assets_path
+module "home_masuria_hosting" {
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.masuria.public_path
+  content_version = var.masuria.content_version
 }
 
-resource "terratowns_home" "home" {
-  name = "How to spend your free time"
-  description = <<DESCRIPTION
+resource "terratowns_home" "home-masuria" {
+  name            = "How to spend your free time"
+  description     = <<DESCRIPTION
 Explore the breathtaking lakes, lush forests, and charming villages that make up the Masurian area.
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
-  town = "missingo"
-  content_version = 2
+  domain_name     = module.home_masuria_hosting.cloudfront_url
+  town            = "missingo"
+  content_version = var.masuria.content_version
+}
+
+module "home_heroes_hosting" {
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.heroes.public_path
+  content_version = var.heroes.content_version
+}
+
+resource "terratowns_home" "home-heroes" {
+  name            = "Are you looking for a game?"
+  description     = <<DESCRIPTION
+Try this one!!! "Heroes of Might and Magic" is a beloved turn-based strategy game
+DESCRIPTION
+  domain_name     = module.home_heroes_hosting.cloudfront_url
+  town            = "missingo"
+  content_version = var.heroes.content_version
 }
